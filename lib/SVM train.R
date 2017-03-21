231db7c47389c8022771d991c0b58dd72c9a891b
CatherineTang <yt2547@columbia.edu>
2017-03-21 17:09:06 -0400

    tuned

--- lib/SVM train.R
+++ lib/SVM train.R
@@ -1,3 +1,5 @@
+
+
 setwd("D:/study/Applied Data Sci/spr2017-proj3-group11/data/training_data/training_data/sift_features")
 ######################################################################
 
@@ -13,13 +15,6 @@ Data[,1] = as.factor(Data[,1])
 names = c("y",paste0("Fea",1:dim(Fea)[2]))
 colnames(Data) = names
 
-#delete the feature that has 0 variance  
-M=apply(Data,2,mean) #mean of M = 3e-4  
-V = apply(Data,2,var)  
-sum(sqrt(V)>3e-4) #3773 features  
-Data = Data[,sqrt(V)>3e-4]  
-
-
 #install.packages("e1071")
 library("e1071")
 ###################################################################
@@ -30,36 +25,30 @@ Train = Data[index,]
 Test = Data[-index,]
 
 #############################
+## SVM linear tune (cross validation)
+## tune.linear <- tune.svm(x =Train[,-1] ,y =Train[,1], cost = seq(100,1000,100),type="C-classification", kernel = "linear",sampling="cross",cross=5)
+## best.svm.linear <- tune.linear$best.model
+## print(best.svm.linear)
+#############################
 
 #start the clock
 ptm <- proc.time()
-## SVM linear tune (cross validation)
-tune.linear <- tune.svm(x =Train[,-1] ,y =Train[,1], cost = c(1,10),type="C-classification", kernel = "linear",sampling="cross",cross=10)
-best.svm.linear <- tune.linear$best.model
-print(best.svm.linear)
+## SVM linear 
+linear.svm<-svm(x =Train[,-1] ,y =Train[,1], cost = 100,type="C-classification", kernel = "linear",sampling="cross",cross=5)
 # Stop the clock
-proc.time() - ptm #50sec
+proc.time() - ptm 
+
+##Train error
+pred.svm.linear <- predict(linear.svm, Train[,-1]) 
+# test error
+train.error.linear <- sum(pred.svm.linear != Train[,1]) / length(Train[,1]) 
+print(train.error.linear)
 
 ##predict
-pred.svm.linear <- predict(best.svm.linear, Test[,-1]) 
+pred.svm.linear <- predict(linear.svm, Test[,-1]) 
 # test error
 error.linear <- sum(pred.svm.linear != Test[,1]) / length(Test[,1]) 
 print(error.linear)
 
-####################################
 
-#start the clock
-ptm <- proc.time()
-## SVM nonlinear RBF tune (cross validation)
-tune.rbf <- tune.svm(x =Train[,-1] ,y =Train[,1], cost = c(1,5),gamma =seq(0.01,0.1,0.03),type="C-classification", kernel = "radial",sampling="cross",cross=10)
-best.svm.rbf <- tune.rbf$best.model
-print(best.svm.rbf)
-# Stop the clock
-proc.time() - ptm 
-
-##predict
-pred.svm.rbf <- predict(best.svm.rbf, Test[,-1]) 
-# test error
-error.rbf <- sum(pred.svm.rbf != Test[,1]) / length(Test[,1]) 
-print(error.rbf)
 
